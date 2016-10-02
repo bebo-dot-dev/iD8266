4f10b43c6859200215dae2a1c0c2b65445b4bed6
joe <joe@jjssoftware.co.uk>
2016-10-02 19:24:15 +0100

    git support added to eclipse

--- .ino.cpp
+++ .ino.cpp
@@ -2,7 +2,7 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2016-09-29 20:20:56
+//This file has been generated on 2016-10-02 19:17:13
 
 #include "Arduino.h"
 #include "flashAppData.h"
