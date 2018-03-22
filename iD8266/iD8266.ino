80dfb93d44da5811765cabf25f1999a649369039
Joe <joe@jjssoftware.co.uk>
2018-03-22 22:05:01 +0000

    arduino IDE compatibility restructure

---
+++ iD8266/iD8266.ino
@@ -0,0 +1,4 @@
+/*
+    This file is intentionally empty and only exists to enable compilation / maintenance to work in the Arduino IDE
+    The actual setup and loop functions exist in iD8266.cpp
+*/
